a57b6bcc11dec49a7ee3c24893aa51108c6c3d4f
AztyMatt <mpetit79@myges.fr>
2025-04-29 00:32:00 +0200

    Revert "feat (wip): refactor navigation structure to use expo-router"
    This reverts commit 0ad4d467f9362ba81931a308993ef08006466766.

--- app/_layout.js
+++ app/_layout.js
@@ -1,23 +1,10 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView } from 'react-native';
-import { Tabs } from 'expo-router';
-import { EventProvider } from 'react-native-outside-press';
-import { GlobalProvider } from '@/GlobalContext.js';
-import Theme from '@/assets/styles.js';
+import { Slot } from 'expo-router';
 
-const Layout = () => {
-  return (
-    <EventProvider>
-      <GlobalProvider>
-        <SafeAreaView style={{ backgroundColor: Theme.colors.secondaryDarker }}>
-          <StatusBar barStyle="light-content" backgroundColor={Theme.colors.secondaryDarker} />
-        </SafeAreaView>
-        {/* expo-router gère automatiquement la navigation */}
-        <Tabs />
-      </GlobalProvider>
-    </EventProvider>
-  );
-};
-
-export default Layout;
+export default function Layout() {
+    return (
+        <>
+            {/* Place any common layout code here */}
+            <Slot />
+        </>
+    );
+}
